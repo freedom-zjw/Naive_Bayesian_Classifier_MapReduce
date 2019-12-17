58b295c76892f55d183d63b05241454b7cefd970
freedom-zjw <709075442@qq.com>
2019-12-17 15:28:16 +0800

    fix train code logic error

--- MapReduce_code/train_mapper.py
+++ MapReduce_code/train_mapper.py
@@ -3,21 +3,15 @@ import sys
 输入：
 label  title
 输出：
-<<label, Nei, word>, 1>
+<label, <word, 1>>
+<label, 1> 
 Nei[label]: 标签为label的title数
 """
-Nei = dict()
-titles = []
-for row in sys.stdin: # 先计算Nei
-    titles.append(row)
-    label, title = row.split('\t\t')
-    if label not in Nei:
-        Nei[label] = 0
-    Nei[label] += 1
-
-for row in titles: # 得到输出格式
+for row in sys.stdin:
     label, title = row.split('\t\t')
     title = title.split()
+    flag = False
     for word in title:
-        key_value = "{}\t\t{}".format(label + ' ' + str(Nei[label]) + ' ' + word, str(1))
+        key_value = "{}\t\t{}".format(label, word + ' ' + str(1))
         print(key_value)
+    print("{}\t\t{}".format(label, str(1)))
--- MapReduce_code/train_reducer.py
+++ MapReduce_code/train_reducer.py
@@ -1,21 +1,31 @@
 import sys
 """
 输入：
-<<label, Nei, word>, 1>
+<label, <word, 1>>
+<label, 1> 
 输出：
 <<label>, <0, Nei, word, numofword>>
 统计标签为label的title中 单词word的出现次数
 """
-Hash = dict()
+wordHash = dict()  
+# 键值为（label, word）的字典，统计标签为label的title中单词word出现的次数
+titleHash = dict()
+# 键值为label的字典，统计标签为label的title个数
 for row in sys.stdin:
     key, value = row.split('\t\t')
-    if key not in Hash:
-        Hash[key] = int(value)
-    else:
-        Hash[key] += int(value)
+    value = value.split()
+    if len(value) == 1:
+        if key not in titleHash:
+            titleHash[key] = 0
+        titleHash[key] += int(value[0])
+    elif len(value) == 2:
+        newkey = key + ' ' + value[0]
+        if newkey not in wordHash:
+            wordHash[newkey] = 0
+        wordHash[newkey] += int(value[1])
 
-for k,v in Hash.items():
+for k,v in wordHash.items(): # 得到输出格式
     k = k.split(' ')
     key = k[0]
-    value = str(0) + ' ' + k[1] + ' ' + k[2] + ' ' + str(v)
+    value = str(0) + ' ' + str(titleHash[key]) + ' ' + k[1] + ' ' + str(v)
     print(key + "\t\t" + value)
